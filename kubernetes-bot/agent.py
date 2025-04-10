12162c34dd2003bee9421d90295e85fe9486a0f6
sayed-imran <sayedimran00786@gmail.com>
2025-04-10 22:38:19 +0530

    Kubernetes Bot using Google ADK

---
+++ kubernetes-bot/agent.py
@@ -0,0 +1,12 @@
+# my_agent/agent.py
+from google.adk.agents import Agent
+from .tools.tools import *
+
+root_agent = Agent(
+    name="kubernetes_assistant",
+    model="gemini-2.0-flash-exp",
+    instruction="You are a helpful assistant. Who can perform tasks of Kubernetes cluster management such as listing namesspaces, deployemnts, pods, and services. Answer the user's questions using the tools available. Answer all the question in Markdown format.",
+    description="An assistant that can help you with your Kubernetes cluster",
+    tools=[list_namespaces, list_deployments_from_namespace, list_pods_from_namespace, list_services_from_namespace, list_all_resources],
+)
+
